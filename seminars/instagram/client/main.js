9363979d34fa362aaacc04adbd1116a6e50c217a
Andrew Sautin <sautin1@yandex.com>
2019-03-30 13:57:00 +0300

    Add instagram project

---
+++ seminars/instagram/client/main.js
@@ -0,0 +1,23 @@
+let Vue = require("vue");
+let VueResource = require("vue-resource");
+let VueRouter = require("vue-router");
+
+let App = require("./components/app.vue");
+let Register = require("./components/register.vue");
+
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+let router = new VueRouter({
+    routes: [
+        { path: "/register", component: Register }
+    ]
+});
+
+let vm = new Vue({
+    el: "#root",
+    router: router,
+    render: function(createElement) {
+        return createElement(App);
+    }
+});
